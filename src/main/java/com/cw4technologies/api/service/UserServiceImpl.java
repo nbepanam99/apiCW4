8ca6d4b53deb0872159946bad2a4400c3b3be5cd
nbepanam99 <nbepanam@gmail.com>
2022-03-08 16:31:36 +0100

    update service

---
+++ src/main/java/com/cw4technologies/api/service/UserServiceImpl.java
@@ -0,0 +1,48 @@
+package com.cw4technologies.api.service;
+
+import com.cw4technologies.api.domain.AppUser;
+import com.cw4technologies.api.domain.Role;
+import com.cw4technologies.api.repo.RoleRepository;
+import com.cw4technologies.api.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service @RequiredArgsConstructor @Transactional @Slf4j
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+
+
+    @Override
+    public AppUser saveUser(AppUser user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Role saveRole(Role role) {
+        return roleRepository.save(role);
+    }
+
+    @Override
+    public void addRoleToUser(String userName, String roleName) {
+        AppUser appUser = userRepository.findByUsername(userName);
+        Role role = roleRepository.findByName(roleName);
+        appUser.getRoles().add(role);
+
+    }
+
+    @Override
+    public AppUser getUser(String userName) {
+        return userRepository.findByUsername(userName);
+    }
+
+    @Override
+    public List<AppUser> getUsers() {
+        return userRepository.findAll();
+    }
+}
