e1d0407a3960d17b4a2dccb2ce06224272e044d8
nbepanam99 <nbepanam@gmail.com>
2022-03-08 16:01:12 +0100

    init project

---
+++ src/test/java/com/cw4technologies/api/MainApplicationTest.java
@@ -0,0 +1,15 @@
+package com.cw4technologies.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+    class MainApplicationTest {
+
+        @Test
+        void contextLoads() {
+        }
+
+    }
+
